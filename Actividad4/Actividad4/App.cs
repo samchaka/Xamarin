24e6b105785ea2d45c75ed3ee1d29ce7c01a8b88
samchaka <samchaka@gmail.com>
2015-09-17 09:51:03 -0500

    Se cambió a inicializadores de objetos en lugar depropiedades.

--- Actividad4/Actividad4/App.cs
+++ Actividad4/Actividad4/App.cs
@@ -8,19 +8,19 @@ namespace Actividad4
 		public static Page GetMainPage ()
 		{	
 			//Se crea un objeto etiqueta
-			Label label = new Label();
+			//Label label = new Label();
 
 			//Se modifican sus propiedades
 			//Texto
-			label.Text = "Cámbiame";
+			//label.Text = "Cámbiame";
 			//Color del texto
-			label.TextColor = Color.Blue;
+			//label.TextColor = Color.Blue;
 			//Alineación vertical (YAlign). Para la alineación horizontal, usar XAlign
-			label.YAlign = TextAlignment.Center;
+			//label.YAlign = TextAlignment.Center;
 
 
-			//El mismo código usando inicializador de objetos
-			//Label label = new Label{ Text = "Cámbiame", TextColor =  Color.Blue, YAlign = TextAlignment.Center };
+			//El mismo código usando inicializador de objetos(Este es el cambio)
+			Label label = new Label{ Text = "Cámbiame", TextColor =  Color.Blue, YAlign = TextAlignment.Center };
 
 			//Se crea una página y se le asigna como contenido la eqtiqueta que se creó
 			ContentPage contentPage = new ContentPage();
