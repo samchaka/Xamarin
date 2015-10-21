aa282d84259c733d2e3ca93892ca83d502371b1d
samchaka <samchaka@gmail.com>
2015-10-21 09:05:36 -0500

    * Actividad9.sln: Se validan datos contraseña usuario "clave" y se   accede a página contenido.
    * Login.cs:
    * Contenido.cs:

--- Actividad9/Actividad9/Contenido.cs
+++ Actividad9/Actividad9/Contenido.cs
@@ -7,6 +7,26 @@ namespace Actividad9
 	{
 		public Contenido ()
 		{
+			Label texto = new Label {
+				Text = "Esta es la pagina de contenido!!!",
+				TextColor = Color.White
+			};
+
+			//Stacklayout permite apilar los controles verticalmente
+			StackLayout stackLayout = new StackLayout
+			{
+				Children =
+				{
+					texto,
+
+				}
+				};
+
+
+			//Como esta clase hereda de ContentPage, podemos usar estas propiedades directamente
+			this.Content = stackLayout;
+			this.Padding = new Thickness (5, Device.OnPlatform (20, 5, 5), 5, 5);
+
 
 		}
 	}
