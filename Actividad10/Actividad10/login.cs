bfb4e95886f2dedba9402c45529b8f2457cb4bc4
samchaka <samchaka@gmail.com>
2015-10-26 09:33:22 -0600

    Se agregó login para acceso a contenido

---
+++ Actividad10/Actividad10/login.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Actividad10
+{
+	public class login: ContentPage
+	{
+		public login ()
+		{
+			Entry usuario = new Entry { Placeholder = "Usuario" };
+			Entry clave = new Entry { Placeholder = "Clave", IsPassword = true };
+
+			Button boton = new Button {
+				Text = "Login",
+				TextColor = Color.White,
+				BackgroundColor = Color.FromHex ("77D065") 
+			};
+
+			boton.Clicked += (sender, e) => {
+
+				if(usuario.Text.Equals("Usuario")){
+					Navigation.PushAsync(new Contenido());
+				}
+			};
+
+			//Stacklayout permite apilar los controles verticalmente
+			StackLayout stackLayout = new StackLayout
+			{
+				Spacing = 20, 
+				Padding = 50,
+				VerticalOptions = LayoutOptions.Center,
+				Children =
+				{
+					usuario,
+					clave,
+					boton
+				}
+				};
+
+			//Como esta clase hereda de ContentPage, podemos usar estas propiedades directamente
+			this.Content = stackLayout;
+			this.Padding = new Thickness (5, Device.OnPlatform (20, 5, 5), 5, 5);
+
+		}
+	}
+}
+
